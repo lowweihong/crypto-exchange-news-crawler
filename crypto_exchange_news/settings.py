7b609ed85af4ae21d75542591bc0111d58a31223
Low Wei Hong <M140042@e.ntu.edu.sg>
2025-06-02 22:17:55 -0400

    added proxy midlleware

--- crypto_exchange_news/settings.py
+++ crypto_exchange_news/settings.py
@@ -56,9 +56,9 @@ PROXY_LIST = []
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    'crytpo_exchange_news.middlewares.MyProxyMiddleware': 543
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     'crytpo_exchange_news.middlewares.MyProxyMiddleware': 543
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
